469e5856dab04f1dafcc135fd644811000b15dff
turashbayevajr <103361172+turashbayevajr@users.noreply.github.com>
2023-01-09 01:44:29 +0600

    Initial Commit

---
+++ tuimedaq/tuimedaqApp.swift
@@ -0,0 +1,17 @@
+//
+//  tuimedaqApp.swift
+//  tuimedaq
+//
+//  Created by Ажар Турашбаева on 09.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct tuimedaqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
